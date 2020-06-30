13d07fe58ea481b4aa887e9fc59b6b96b518ff16
Lance Newman <lnewman@brightidea.com>
2020-06-30 19:28:20 -0400

    Updated serverless function

--- api/contact/send.js
+++ api/contact/send.js
@@ -1,4 +1,5 @@
 /* eslint-disable */
+const Mail = require("../../utils/mailFunctions.js")
 const nodemailer = require("nodemailer")
 
 module.exports = async (req, res) => {
@@ -12,42 +13,23 @@ module.exports = async (req, res) => {
 	const subject = "Someone has contacted you"
 	const text = msg
 	const html = msg
+	Mail.sendEmail(to, subject, text, html)
 
-	const transporter = nodemailer.createTransport({
-		auth: {
-			user: "noreply@alliesonly.com",
-			pass: "t3hT!Pvs94wt1Wk"
-		},
-		host: "smtpout.secureserver.net",
-		port: 465,
-		requireTLS: true,
-		secure: true,
-		secureConnection: false,
-		tls: {
-			ciphers: "SSLv3"
-		}
-	})
+	try {
+		const to = "lnlance09@gmail.com"
+		const subject = "Someone has contacted you"
+		const text = msg
+		const html = msg
+		Mail.sendEmail(to, subject, text, html)
 
-	await transporter.sendMail(
-		{
-			from: '"Allies Only" <noreply@alliesonly.com>',
-			to,
-			subject,
-			text,
-			html
-		},
-		(error) => {
-			if (error) {
-				return res.status(500).send({
-					error: true,
-					msg: "There was an error"
-				})
-			} else {
-				return res.status(200).send({
-					error: false,
-					msg: "You message has been sent"
-				})
-			}
-		}
-	)
+		return res.status(200).send({
+			error: false,
+			msg: "You message has been sent"
+		})
+	} catch (err) {
+		return res.status(500).send({
+			error: true,
+			msg: "There was an error"
+		})
+	}
 }
