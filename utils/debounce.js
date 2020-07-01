3baf5220ecb2f3466155ee3602a1169e48c74bb8
Lance Newman <lnewman@brightidea.com>
2020-06-30 20:24:34 -0400

    Fixed ts error

--- utils/debounce.js
+++ utils/debounce.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react"
 
-export const useDebounce = (value, delay) => {
+export default function useDebounce(value, delay) {
 	const [debouncedValue, setDebouncedValue] = useState(value)
 
 	useEffect(() => {
