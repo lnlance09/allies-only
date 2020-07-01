e431b2588f9b39aa2ce1db6ee88f6859c7875bb7
Lance Newman <lnewman@brightidea.com>
2020-06-30 20:22:15 -0400

    Fixed ts error

--- utils/textFunctions.js
+++ utils/textFunctions.js
@@ -1,4 +1,4 @@
-export const formatPlural = (count: number, term: string): string => {
+export const formatPlural = (count, term) => {
 	if (term.substr(term.length - 1) === "y") {
 		const word = term.substring(0, term.length - 1)
 		return parseInt(count, 10) === 1 ? term : `${word}ies`
