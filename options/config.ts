c9e2a559ab7bca0e38d2243262d327c2a44eb1a9
Lance Newman <lnewman@brightidea.com>
2020-07-03 16:21:50 -0400

    changed base url

--- options/config.ts
+++ options/config.ts
@@ -1,4 +1,4 @@
-export const baseUrl = "http://127.0.0.1:3000/"
+export const baseUrl = "http://ec2-54-218-105-47.us-west-2.compute.amazonaws.com/"
 export const s3BaseUrl = "https://alliesonly.s3-us-west-2.amazonaws.com/"
 // export const s3BaseUrl = "https://alliesonly.s3-accelerate.amazonaws.com/"
 export const siteName = "Allies Only"
