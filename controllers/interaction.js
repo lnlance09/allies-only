99529eb12cbb6e38696101ab88f50c57887b5ebb
Lance Newman <lnewman@brightidea.com>
2020-07-03 18:32:47 -0400

    Bug fix

--- controllers/interaction.js
+++ controllers/interaction.js
@@ -652,7 +652,7 @@ exports.uploadVideo = async (req, res) => {
 	}
 
 	const { file } = req.files
-	const ext = path.extname(file.name)
+	const ext = path.extname(file.name).toLowerCase()
 	const extensions = [".avi", ".flv", ".m4v", ".mp4", ".mkv", ".mov", ".webm"]
 
 	if (!extensions.includes(ext)) {
