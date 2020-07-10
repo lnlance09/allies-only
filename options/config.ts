8c296d7fe262b6b5d3c0285a1da46e0dd9eea62c
Lance Newman <lnewman@brightidea.com>
2020-07-10 13:35:08 -0400

    base url update

--- options/config.ts
+++ options/config.ts
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
-export const baseUrl = "http://ec2-54-218-105-47.us-west-2.compute.amazonaws.com/"
-export const s3BaseUrl = "http://alliesonly.com/"
-// export const s3BaseUrl = "https://alliesonly.s3-us-west-2.amazonaws.com/"
-=======
 // export const baseUrl = "http://127.0.0.1:3000/"
 export const baseUrl = "http://alliesonly.com/"
 export const s3BaseUrl = "https://alliesonly.s3-us-west-2.amazonaws.com/"
->>>>>>> develop
 // export const s3BaseUrl = "https://alliesonly.s3-accelerate.amazonaws.com/"
 export const siteName = "Allies Only"
