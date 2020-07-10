9cccd5c5117c316a5675a3f90d7a3b431def8201
Lance Newman <lnewman@brightidea.com>
2020-07-10 12:53:48 -0400

    Changed remote url

--- options/config.ts
+++ options/config.ts
@@ -1,4 +1,5 @@
 export const baseUrl = "http://ec2-54-218-105-47.us-west-2.compute.amazonaws.com/"
-export const s3BaseUrl = "https://alliesonly.s3-us-west-2.amazonaws.com/"
+export const s3BaseUrl = "http://alliesonly.com/"
+// export const s3BaseUrl = "https://alliesonly.s3-us-west-2.amazonaws.com/"
 // export const s3BaseUrl = "https://alliesonly.s3-accelerate.amazonaws.com/"
 export const siteName = "Allies Only"
